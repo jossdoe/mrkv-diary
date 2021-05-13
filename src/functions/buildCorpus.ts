e3862898cd7b8b9db399dbf0de51678bba580af0
Joss Doebler <mail@joss-doebler.de>
2021-05-13 13:38:19 +0200

    docs: comment buildCorpus-function

--- src/functions/buildCorpus.ts
+++ src/functions/buildCorpus.ts
@@ -8,16 +8,19 @@ export function buildCorpus(textInput: Source): Corpus {
   const wordsArray = textInput.split(' ');
 
   return wordsArray.reduce((acc, currentValue, currentIndex) => {
+    // Stop creating pairs, once we run out of sufficent words for a prefix/suffix pair
     if (currentIndex > wordsArray.length - 3) return acc;
 
     const prefix = `${currentValue} ${wordsArray[currentIndex + 1]}`;
     const suffix = wordsArray[currentIndex + 2];
 
+    // Check if prefix already has an entry and push to its suffixes-array
     if (acc.get(prefix)) {
       acc.get(prefix).push(suffix);
       return acc;
     }
 
+    // Create new prefix entry
     acc.set(prefix, [suffix]);
     return acc;
   }, new Map());
